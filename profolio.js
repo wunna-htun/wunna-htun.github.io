18eb357acd7e2e5ed0b98b2306e1f6d0d39232bb
Wunna Htun <wunnahtun@blue-stone.net>
2024-12-07 16:28:00 +0700

    [mod] toggle btn in mobile size devices

--- profolio.js
+++ profolio.js
@@ -42,3 +42,12 @@ themeToggleBtn.addEventListener('click', function() {
     }
     
 });
+
+
+// Mobile menu toggle
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileMenuButton.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
